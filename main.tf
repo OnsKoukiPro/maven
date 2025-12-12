f2a492fa19cc0f89f276262f98e5c43fabca7b70
OnsKoukiPro <ons.koukipro@yahoo.com>
2025-12-12 16:04:10 +0100

    cleanup

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "docker" {}
 
-# --- 1. Ressource : Base de Données PostgreSQL ---
+# - Ressource : Base de Données PostgreSQL ---
 
 # Télécharge l'image PostgreSQL depuis Docker Hub
 resource "docker_image" "postgres_image" {
@@ -18,7 +18,6 @@ resource "docker_image" "postgres_image" {
   keep_locally = true
 }
 
-# Crée et configure le conteneur PostgreSQL
 resource "docker_container" "db_container" {
   name  = "tp-db-postgres"
   image = docker_image.postgres_image.image_id  # Utilise .image_id pour v3.x
@@ -28,7 +27,6 @@ resource "docker_container" "db_container" {
     external = 5432
   }
 
-  # Configuration de la DB via les variables d'environnement
   env = [
     "POSTGRES_USER=${var.db_user}",
     "POSTGRES_PASSWORD=${var.db_password}",
@@ -36,7 +34,7 @@ resource "docker_container" "db_container" {
   ]
 }
 
-# --- 2. Ressource : Application Web Nginx ---
+# --- 2.Application Web Nginx ---
 
 # Construit l'image de l'application à partir du Dockerfile_app local
 resource "docker_image" "app_image" {
@@ -48,17 +46,15 @@ resource "docker_image" "app_image" {
   keep_locally = true
 }
 
-# Crée le conteneur de l'application web
 resource "docker_container" "app_container" {
   name  = "tp-app-web"
   image = docker_image.app_image.image_id  # Utilise .image_id pour v3.x
 
-  # Dépendance explicite : la DB doit être prête avant l'Application
+
   depends_on = [
     docker_container.db_container
   ]
 
-  # Mappage du port 80 interne au port externe défini dans variables.tf
   ports {
     internal = 80
     external = var.app_port_external
